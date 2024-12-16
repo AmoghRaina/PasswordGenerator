02add174be80472556684e56f7ce6a96b0533269
AmoghRaina <amoghraina2000@gmail.com>
2024-12-16 11:22:00 +0530

    style changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include <QPixmap>
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -8,6 +8,13 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     w.setWindowTitle("Password Generator by Amogh Raina");
+
+    QPixmap pixmap(":/logo/img/32680994b925140b048e2ee11f03aaf0.png");
+    w.setWindowIcon(QIcon(pixmap));
+     w.setFixedSize(w.size());
     return a.exec();
+
+
+
 
 }
